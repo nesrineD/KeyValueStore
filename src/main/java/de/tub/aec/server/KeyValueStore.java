36f56d4b5a4d03d1425ff7bc835507fe9a9cdb54
nesrineD <nesrine.doghri1@gmail.com>
2015-07-08 14:33:47 +0200

    server

---
+++ src/main/java/de/tub/aec/server/KeyValueStore.java
@@ -0,0 +1,128 @@
+package de.tub.aec.server;
+
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+
+import de.tuberlin.ise.aec.group3.exceptions.KeyAlreadyExistsException;
+import de.tuberlin.ise.aec.group3.exceptions.KeyNotFoundException;
+import de.tuberlin.ise.aec.group3.exceptions.SameValueException;
+import edu.kit.aifb.dbe.hermes.Request;
+import edu.kit.aifb.dbe.hermes.Response;
+import edu.kit.aifb.dbe.hermes.Sender;
+
+public class KeyValueStore {
+	
+	private static Logger logger = Logger.getLogger(KeyValueStore.class);
+	
+	long writeTime;
+
+	/** Singleton instance */
+	private static KeyValueStore instance;
+	
+	/** The actual key-value store */
+	private HashMap<String, byte[]> store = new HashMap<String, byte[]>();
+	
+	/**
+	 * Make sure that only one instance exists.
+	 *
+	 * @return the singleton
+	 */
+	public static KeyValueStore getInstance() {
+		if (KeyValueStore.instance == null) {
+			KeyValueStore.instance = new KeyValueStore();
+		}
+		return instance;
+	}
+	
+	/**
+	 * Add a new value to the store.
+	 *
+	 * @param key
+	 * @param value
+	 * @throws KeyAlreadyExistsException
+	 */
+	public void create(String key, byte[] value)
+			throws KeyAlreadyExistsException {
+		/** Key does not yet exists, entry can be added */
+		if (!store.containsKey(key)) {
+			store.put(key, value);
+		} else {
+			throw new KeyAlreadyExistsException("Key already exists in the store.");
+		}
+	}
+	
+	/**
+	 * Read a value from the store
+	 *
+	 * @param key
+	 * @return the value
+	 * @throws KeyNotFoundException
+	 */
+	public byte[] read(String key)
+			throws KeyNotFoundException {
+		/** Check, if the key exists in the store */
+		if (store.containsKey(key)) {
+			return store.get(key);
+		} else {
+			throw new KeyNotFoundException("Key does not exist in the store.");
+		}
+	}
+	
+	/**
+	 * Delete a value from the store.
+	 *
+	 * @param key
+	 * @throws KeyNotFoundException
+	 */
+	public void delete(String key)
+			throws KeyNotFoundException {
+		/** Check, if the key exists in the store */
+		if (store.containsKey(key)) {
+			store.remove(key);
+		} else {
+			throw new KeyNotFoundException("Key does not exist in the store.");
+		}
+	}
+
+	/**
+	 * @param host ip address to which the client is reachable
+	 * @param port port number of the client
+	 * @param req
+	 * @return
+	 */
+	private static Response sendMessage(String host, int port, Request req) {
+		Sender s = new Sender(host, port);
+		return s.sendMessage(req, 100000);
+	}
+	
+	/**
+	 * Update an old value with a new one.
+	 *
+	 * @param key
+	 * @param value
+	 * @throws KeyNotFoundException
+	 * @throws SameValueException
+	 */
+	public void update(String key, byte[] value)
+			throws KeyNotFoundException, SameValueException {
+		/** Check, if the key exists in the store */
+		if (store.containsKey(key)) {
+			if (!store.get(key).equals(value)) {
+				store.replace(key, value);
+				writeTime = System.currentTimeMillis();
+				logger.info("the write time is" + writeTime);
+				// send the time of the write to the client
+				Request req = new Request("writeTime", "nodeA");
+				req.addItem(writeTime);
+				Response resp = sendMessage("localhost", 6000, req);
+				logger.info("the response is to A" + resp + "message sent");
+				
+			} else {
+				throw new SameValueException("Value has not changed.");
+			}
+		} else {
+			throw new KeyNotFoundException("Key does not exist in the store.");
+		}
+	}
+}
