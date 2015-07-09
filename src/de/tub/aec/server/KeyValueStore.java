be15e20554a99a9e5cb979edbd27330ca3dea670
nesrineD <nesrine.doghri1@gmail.com>
2015-07-09 04:29:03 +0200

    classes

--- src/de/tub/aec/server/KeyValueStore.java
+++ src/de/tub/aec/server/KeyValueStore.java
@@ -113,10 +113,10 @@ public class KeyValueStore {
 				writeTime = System.currentTimeMillis();
 				logger.info("the write time is" + writeTime);
 	           // send the time of the write to the client
-				Request req = new Request("writeTime", "nodeB");
+				Request req = new Request("writeTime", "nodeC");
 				req.addItem(writeTime);
 				Response resp = sendMessage("89.204.135.19", 6000, req);
-				logger.info("the response is to B" + resp + "message sent");
+				logger.info("the response is to C" + resp + "message sent");
 				
 			} else {
 				throw new SameValueException("Value has not changed.");
